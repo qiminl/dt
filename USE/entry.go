761a75e0e8f11397d5fb8af7e24c97b5178b9419
Edward Guan <gsngsngsn@gmail.com>
2017-01-06 09:19:05 -0800

    fraudlogix detec

--- USE/entry.go
+++ USE/entry.go
@@ -13,6 +13,8 @@ import (
 )
 
 var(
+	fraudlogix_csv = "/Users/edward/Downloads/etoron-results/medium.csv"
+
 	count int;
 	date = "2016-12-29"
 	folder_base ="/Users/edward/work/backup/"
@@ -134,6 +136,57 @@ func Output_Json (dir string){
  	fmt.Printf("load data time cost %v\n",t2.Sub(start)) 
 }
 
+//search against fraudlogix list. 
+func search_Object (dir string, csv_path string) map[string] int{
+
+	var result_app map[string] int
+	result_app = make(map[string] int)
+	var result_time map[string] int
+	result_time = make(map[string] int)
+
+	var ip_list map[string] int
+	ip_list = make(map[string] int)
+	ip_list = dt.Read_from_fraudlogix_csv(csv_path)
+
+	var map_problem_ip_object map[string] []*dt.Record
+	map_problem_ip_object = make(map[string] []*dt.Record)
+
+	start := time.Now()
+	hmm, _ := ioutil.ReadDir(dir)
+	fmt.Println("Reading DIR=",dir)
+    for _, f := range hmm {
+	    rl := &[]dt.Record{}
+
+		date = f.Name()
+    	folder = folder_base+ date
+		count = dt.Read_Records_From_Folder(rl, folder) 
+		fmt.Println("rl: ", len(*rl), "; count=",count)
+		//dt.Write_json_Array(folder_Ouputs+date+".json", rl)
+		for _, f := range *rl {
+ 			//fmt.Println(" rl.ip=", f.Ip)
+ 			if ip_list[f.Ip] >=1{
+ 				interm, _ := strconv.ParseInt(f.Time, 10, 64)
+				date := time.Unix(interm, 0).String()
+ 				fmt.Println("catch bad ip=",f.Ip, " for app_id=",f.App_id, " at ", date)
+ 				map_problem_ip_object[f.Ip] = append(map_problem_ip_object[f.Ip], &f)
+
+ 				result_app[f.App_id] +=1
+ 				date_only := strings.Fields(date)
+ 				result_time[date_only[0]]+=1
+ 			}
+ 		}
+
+	}
+	t2 := time.Now()
+ 	fmt.Printf("load data time cost %v\n",t2.Sub(start)) 
+
+	for k,v := range result_time{
+		fmt.Println("@ ",k," there is #",v," of views&clicks with high risk")
+	}
+
+ 	return result_app
+}
+
 func main(){
 
 	//test_reader()
@@ -142,12 +195,21 @@ func main(){
 	//read_dir_IPs(folder_base)
 
 	//Output_Json (folder_base)
+	var result_app map[string] int
+	result_app = make(map[string] int)
+	result_app = search_Object(folder_base, fraudlogix_csv)
 
-	date ="2017-01-04"
-	rl := &[]dt.Record{}
-	folder = folder_base + date
-	count = dt.Read_Records_From_Folder(rl, folder) 
-	fmt.Println("rl: ", len(*rl), "; count=",count)
-	dt.Write_json_Array(folder_Ouputs+date+".json", rl)
+	for k,v := range result_app{
+		fmt.Println("app#id=",k," has #",v," of views&clicks with ",fraudlogix_csv," risk")
+	}
+
+
+
+	// date ="2017-01-04"
+	// rl := &[]dt.Record{}
+	// folder = folder_base + date
+	// count = dt.Read_Records_From_Folder(rl, folder) 
+	// fmt.Println("rl: ", len(*rl), "; count=",count)
+	// dt.Write_json_Array(folder_Ouputs+date+".json", rl)
 
 }
